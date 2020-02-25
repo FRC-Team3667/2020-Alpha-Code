bd723ee7ef4e7ac480c5d6a3e08a8a5aedb1f570
trevorxarcieri <56984321+trevorxarcieri@users.noreply.github.com>
2020-02-24 20:36:34 -0500

    Added Operations class
    The Operations class was added in order to provide a location for the various non-drive operations of the robot: retrieving power cells, internally indexing power cells, spinning the shield generator, spinning to a certain color on the shield generator, operating drive motors in order to climb, etc.

---
+++ src/main/java/frc/robot/Operations.java
@@ -0,0 +1,32 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+/**
+ * Add your docs here.
+ */
+public class Operations {
+    //motor variables
+    //VictorSPX controllers are smaller than TalonSRX controllers
+    
+
+    public Operations()
+    {
+        //motor variable initialization
+
+    }
+
+    public void operate(Joystick j)
+    {
+
+    }
+}
